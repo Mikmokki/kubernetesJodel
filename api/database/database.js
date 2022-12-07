695193ed4794469288e583fb988d1d8b84954dae
Mikko Juusti <mikko.juusti@aalto.fi>
2022-12-07 20:47:48 +0200

    Change configs

--- api/database/database.js
+++ api/database/database.js
@@ -1,7 +1,15 @@
 import { Pool } from "../deps.js";
 
 const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
+const PGPASS = Deno.env.get("PGPASS");
+const PGPASS_PARTS = PGPASS.split(":");
+const hostname = PGPASS_PARTS[0];
+const port = PGPASS_PARTS[1];
+const database = PGPASS_PARTS[2];
+const user = PGPASS_PARTS[3];
+const password = PGPASS_PARTS[4];
+console.log(hostname, port, database, user, password)
+const connectionPool = new Pool({ hostname, port, database, user, password }, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, params) => {
   const response = {};
