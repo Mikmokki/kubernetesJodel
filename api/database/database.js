517e72387ec3b645ae18dff334a2defe0828882c
Mikko Juusti <mikko.juusti@aalto.fi>
2022-12-08 20:30:39 +0200

    Modify kubernetes configurations

--- api/database/database.js
+++ api/database/database.js
@@ -1,14 +1,13 @@
 import { Pool } from "../deps.js";
 
 const CONCURRENT_CONNECTIONS = 2;
-const PGPASS = Deno.env.get("PGPASS");
+const PGPASS = Deno.env.get("PGPASS").trim();
 const PGPASS_PARTS = PGPASS.split(":");
 const hostname = PGPASS_PARTS[0];
 const port = PGPASS_PARTS[1];
 const database = PGPASS_PARTS[2];
 const user = PGPASS_PARTS[3];
 const password = PGPASS_PARTS[4];
-console.log(hostname, port, database, user, password)
 const connectionPool = new Pool({ hostname, port, database, user, password }, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, params) => {
